f8e6845bb168cf16e9e9f3646d4a51ac6b9b279d
vadimkholodilo <vadimkholodilo@gmail.com>
2024-03-26 22:43:25 -0500

    tring from the command line arguments

--- DataExplorer.Console/Application.cs
+++ DataExplorer.Console/Application.cs
@@ -1,3 +1,5 @@
+using System.Reflection.Metadata.Ecma335;
+
 namespace DataExplorer.Console;
 
 public class Application
@@ -5,7 +7,15 @@ public class Application
 
     public async Task Run(string[] args)
     {
+        // The first argument contains connection string
+        if (args.Length == 0 || string.IsNullOrEmpty(args[0]))
+        {
+            throw new Exception("Connection string was not provided. Pass your connection string as the first argument");
+        }
+
+        string connectionString = args[0];
         System.Console.WriteLine("Welcome to Data Explorer");
+       System.Console.WriteLine($"The following connection string will be used: {connectionString}");
     }
     
 }
